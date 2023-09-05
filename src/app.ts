8b346349fddaed8c199eb3c658ed8fb9ac99e981
Tej Birring <tej@parkourops.com>
2023-09-05 12:38:24 +0100

    fix paths

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { startNewServer } from "@framework";
-import pkg from "./package.json";
+import pkg from "../package.json";
 import postPing from "@routes/postPing";
 
 startNewServer(process.env.PORT ? parseInt(process.env.PORT) : 3000, {
