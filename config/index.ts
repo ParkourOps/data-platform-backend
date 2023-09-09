551b98e85b1dd2ef0d2a053d299161edf789bcd8
Tej Birring <tej@parkourops.com>
2023-09-09 11:27:27 +0100

    add env confs.

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,11 @@
-import { loadOptionalEnvironmentVariable } from "./env-vars";
+import { loadOptionalEnvironmentVariable, loadRequiredEnvironmentVariable } from "./env-vars";
 
 export default {
     PORT: loadOptionalEnvironmentVariable("PORT", "number"),
-    TEST_ACCOUNT_EMAIL: loadOptionalEnvironmentVariable("TEST_ACCOUNT_EMAIL", "string"),
-    TEST_ACCOUNT_PASSWORD: loadOptionalEnvironmentVariable("TEST_ACCOUNT_PASSWORD", "string"),
-    FIREBASE_SPA_API_KEY: loadOptionalEnvironmentVariable("FIREBASE_SPA_API_KEY", "string")
+    TEST_ACCOUNT_EMAIL: loadRequiredEnvironmentVariable("TEST_ACCOUNT_EMAIL", "string"),
+    TEST_ACCOUNT_PASSWORD: loadRequiredEnvironmentVariable("TEST_ACCOUNT_PASSWORD", "string"),
+    FIREBASE_SPA_API_KEY: loadRequiredEnvironmentVariable("FIREBASE_SPA_API_KEY", "string"),
+    NEO4J_URL: loadRequiredEnvironmentVariable("NEO4J_URL"),
+    NEO4J_USERNAME: loadRequiredEnvironmentVariable("NEO4J_USERNAME"),
+    NEO4J_PASSWORD: loadRequiredEnvironmentVariable("NEO4J_PASSWORD"),
 } as const;
